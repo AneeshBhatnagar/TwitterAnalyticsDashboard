92fbdd2df75009892cf22b68869c9132fe6d2e3e
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2017-04-28 14:08:17 -0400

    Completed Initial Frontend

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-$("#button").click(function(){
-	$("#sliderme").slideToggle();
-	console.log("Hello");
-	$("#langChartDiv").css("display","none");
-});
+$(document).ready(function () {
+
+	$("#topic1").hide();
+	$("#topic2").hide();
+
+
+	$("#btn2").click(function(){
+		$("#topic1").fadeToggle();
+		$("#topic2").fadeToggle();
+	});
+	
+	$("#textBox").on('click', function(){
+
+		$("#textBox").val(''); // reseting it to empty
+	});
+ });

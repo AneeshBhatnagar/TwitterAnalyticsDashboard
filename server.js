22ba552549bf2fd5dd81464fe4b626ed5a792451
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2017-05-01 22:38:22 -0400

    PreProcess and Save Tweets in Mongo

--- server.js
+++ server.js
@@ -39,18 +39,37 @@ var geocoder = NodeGeocoder(geoOptions);
 //var geocoder = NodeGeocoder.get('google');
 //Search twitter feed
 
-var params = {screen_name: 'POTUS'};
+//var params = {q: 'Galaxy S8',count: 100, since_id: null};
+//var params = {q: 'Trump',count: "100"};
 function getSearch(params){
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    client.get('search/tweets', params, function(error, tweets, response) {
       if (!error) {
-        //console.log(tweets[0]);
-        tweet = tweets[0];
+        //console.log(tweets.statuses[0]);
+        //process.exit()
+        tweet = tweets.statuses[0];
+        console.log("Fetched", tweets.statuses.length, "tweets for", params.q);
+        for(i=0; i<tweets.statuses.length; i++){
+            var temp = new Tweet;
+            preProcess(tweets.statuses[i],temp);
+        }
+        /*//console.log(tweet);
+        //console.log("User", tweet.user);
+        //console.log("Location", tweet.user.location);
+        //process.exit();
+        count = 0;
+        for(x in tweets.statuses){
+            count++;
+        }
+        console.log("count: ", count);
         console.log(tweet.lang);
         var loc = tweet.user.location;
         if(loc)
         {
-            geocoder.geocode(loc, function(err, res) {
-              console.log(res[0]);
+            geocoder.geocode(loc).then(function(res){
+                console.log(loc);
+                console.log(res[0]);
+            }).catch(function(err){
+                console.log(err);
             });
         }
 
@@ -63,18 +82,20 @@ function getSearch(params){
         var hashtags = tweet.entities.hashtags
         var h = [];
         hashtags.forEach(function(ht){
-            h.push(ht.text);
+            h.push("#"+ht.text.toLowerCase());
         });
         console.log(h);
         var mentions = tweet.entities.user_mentions
         var o = [];
         mentions.forEach(function(m){
-            o.push(m.screen_name)
+            o.push("@"+m.screen_name.toLowerCase())
         });
         console.log(o)
         console.log(tweet.user.verified)
         console.log(tweet.created_at)
-        console.log("Sentiment Score: " + sentiment(tweet.text).score)
+        console.log("Sentiment Score: " + sentiment(tweet.text).score)*/
+      }else{
+        console.log("error");
       }
     });
 }
@@ -93,12 +114,94 @@ function getStream(word){
     });
 }
 //getSearch(params);
-getStream("Trump");
+//getStream("Trump");
 
 // listen (start app with node server.js) ======================================
 app.listen(8080);
 console.log("App listening on port 8080");
 
+var globalSearch;
+var Tweet = mongoose.model('Tweet',{
+    tweet_id: String,
+    source: String,
+    lang: String,
+    hashtags_mentions: [String],
+    location: String,
+    verified: Boolean,
+    date: Date,
+    sentiment: String,
+    searchTerm: String
+});
+
+
+function preProcess(xTweet, t){
+    //console.log("In preProcess");
+    t.tweet_id = xTweet.id;
+    //console.log("ID:",xTweet.id);
+    //console.log(xTweet.source);
+    var s = xTweet.source.match(">([^<]*)<")[0];
+    s = s.substring(1,s.length -1);
+    t.source = s;
+    t.lang = xTweet.lang;
+    var hashtags = xTweet.entities.hashtags;
+    var mentions = xTweet.entities.user_mentions;
+    var h_m = [];
+    hashtags.forEach(function(ht){
+        h_m.push("#"+ht.text.toLowerCase());
+    });
+    mentions.forEach(function(m){
+        h_m.push("@"+m.screen_name.toLowerCase());
+    });
+    t.hashtags_mentions = h_m;
+    t.verified = xTweet.user.verified;
+    t.date = xTweet.created_at;
+    t.sentiment = sentiment(xTweet.text).score;
+    t.searchTerm = globalSearch;
+    if(xTweet.user.location){
+        geocoder.geocode(xTweet.user.location).then(function(res){
+            t.location = res[0];
+            //console.log(t);
+            t.save();
+            //console.log(t);
+        }).catch(function(err){
+            //console.log("NA");
+            t.location = "NA";
+            t.save();
+        });
+    }
+
+    
+}
+
+var params = {q: '',count: "100"};
+
+app.post('/api/searchTweets', function(req,res){
+    globalSearch = req.body.text;
+    params.q = globalSearch;
+    console.log(params);
+    client.get('search/tweets', params, function(error, tweets, response) {
+      if (!error) {
+        tweet = tweets.statuses[0];
+        console.log("Fetched", tweets.statuses.length, "tweets for", params.q);
+        for(i=0; i<tweets.statuses.length; i++){
+            var temp = new Tweet;
+            preProcess(tweets.statuses[i],temp);
+        }
+      }else{
+        console.log(error);
+      }
+    });
+    var resp = {"success":"True"};
+    res.json(resp);
+});
+
+app.get('/api/tweets', function(req, res){
+    //Returns all tweets from the database
+});
+
+app.get("*", function(req,res){
+    res.sendFile(__dirname + '/public/index.html');
+});
+
 //Usage for Sentiment
 console.log(sentiment("cats are stupid").score);
-console.log(sentiment("cats are awesome").score);
